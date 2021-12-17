c6ff9056c20918bf0dbedc29c6412f39bb3590f8
Jatin Kaushik <Jatinchandkaushik@gmail.com>
2021-12-17 18:28:25 +0530

    Remove all prime in arraylist

--- ArrayList/removeallprime.java
+++ ArrayList/removeallprime.java
@@ -1,5 +1,37 @@
 package ArrayList;
 
 public class removeallprime {
-    
+    public static boolean isprime(int number){
+        boolean flag = true;
+        for (int div = 2; div*div<=number; div++){
+            if (number % div == 0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+	public static void solution(ArrayList<Integer> al){
+		// write your code here
+        for(int i = 0; i < al.size(); i++){
+            if(isprime(al.get(i)) == true){
+                al.remove(i);
+                i--;
+            }
+        }
+
+		
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i = 0 ; i < n; i++){
+			al.add(scn.nextInt());
+		}
+		solution(al);
+		System.out.println(al);
+	}
+
 }
