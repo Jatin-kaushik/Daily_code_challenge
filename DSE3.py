006c433c66b98e9e51ce5a968393070d433eb8b7
Jatin-kaushik <Jatinchandkaushik@gmail.com>
2022-01-31 18:21:45 +0530

    Target sum pair java

--- DSE3.py
+++ DSE3.py
@@ -8,7 +8,7 @@ bread_price = int(input())
 avail_cake_flavour = []
 for i in range(n):
     avail_cake_flavour.append(int(input()))
-    
+
 req_cake_flavour = []
 for i in range(n):
     req_cake_flavour.append(int(input()))
@@ -16,14 +16,20 @@ for i in range(n):
 req_cake_floor = []
 for i in range(n):
     req_cake_floor.append(int(input()))
-    
+
 cake_price = []
 for i in range(n):
     cake_price.append(int(input()))
-    
-def max_profit(totalfloor,n, bread_floor,bread_price,avail_cake_flavour,req_cake_flavour,req_cake_floor,cake_price):
+
+
+def max_profit(totalfloor, n, bread_floor, bread_price, avail_cake_flavour, req_cake_flavour, req_cake_floor, cake_price):
     ls = []
-    ls.append(bread_price/bread_floor, 0, 9999999)
-    
-    
-    
+    # pricepergram, bread price, req amt, avail amt, req special flav, avail special flavor
+    ls.append([bread_price/bread_floor,bread_price,  0, 9999999, 0])
+    for i in range(n):
+        ls.append([cake_price[i]/req_cake_floor[i], cake_price[i], req_cake_floor[i],
+                  avail_cake_flavour[i], req_cake_flavour[i]])
+    print(ls)
+
+
+max_profit(totalfloor, n, bread_floor, bread_price, avail_cake_flavour, req_cake_flavour, req_cake_floor, cake_price)
